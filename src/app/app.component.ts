883a6a77e703ad3b2300ccff46a26247f3cef202
Can AKTEPE <can.aktepe@triodor.eu>
2018-06-21 17:15:13 +0300

    responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+  ngOnInit(): void {
+
+
+    $(".btn-toggle").click(function() {
+
+      var wrapper = $('.wrapper');
+    
+    
+      wrapper.toggleClass("closed")
+    })
+    
+
+  }
   title = 'app';
 }
