5cc80423896c5f35782b9c1d1b1779b508c36711
Can AKTEPE <can.aktepe@triodor.eu>
2018-06-22 16:40:29 +0300

    font-awesome updated project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule, NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import * as $ from 'jquery';
 
 @Component({
@@ -6,21 +7,13 @@ import * as $ from 'jquery';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
-
-
   ngOnInit(): void {
-
-
     $(".btn-toggle").click(function() {
-
       var wrapper = $('.wrapper');
-    
-    
       wrapper.toggleClass("closed")
     })
-    
-
   }
   title = 'app';
 }
