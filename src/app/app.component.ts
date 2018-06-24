313801f19de17be94d635da2f2534047399454f6
canaktepe <aktepecan@gmail.com>
2018-06-25 00:38:46 +0300

    weekends updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,22 @@
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+
 import { Component, OnInit } from '@angular/core';
-// import {NgSpinningPreloader} from 'ng2-spinning-preloader';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import { Title } from '@angular/platform-browser';
 import * as $ from 'jquery';
+import {
+  NavigationCancel,
+  Event,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  ActivatedRoute,
+  Router
+} from '@angular/router';
+
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +24,67 @@ import * as $ from 'jquery';
   styleUrls: ['./app.component.css']
 })
 
+
 export class AppComponent implements OnInit {
 
-  // constructor(private ngSpinningPreloader: NgSpinningPreloader) {}
+  header: string;
+  sideBarState: boolean;
+
+  constructor(private loadingBar: SlimLoadingBarService,
+    private router: Router,
+    private titleService: Title,
+    private activatedRoute: ActivatedRoute,
+    private localStorageService: LocalStorageService) {
+
+    this.router.events.subscribe((event: Event) => {
+      this.navigationInterceptor(event);
+    });
+  }
+
+  private navigationInterceptor(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this.loadingBar.start();
+    }
+    if (event instanceof NavigationEnd) {
+      this.loadingBar.complete();
+    }
+    if (event instanceof NavigationCancel) {
+      this.loadingBar.stop();
+    }
+    if (event instanceof NavigationError) {
+      this.loadingBar.stop();
+    }
+  }
+
+  AddLocalStorage(state) {
+
+    this.localStorageService.add("side-bar", state);
+    this.sideBarState = state;
+  }
 
   ngOnInit(): void {
-    // this.ngSpinningPreloader.stop();
+    this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .map(() => this.activatedRoute)
+      .map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      })
+      .filter((route) => route.outlet === 'primary')
+      .mergeMap((route) => route.data)
+      .subscribe((event) => {
+        this.titleService.setTitle(event['title'])
+        this.header = event['title'];
+      });
 
-    $(".btn-toggle").click(function() {
+
+    $(".btn-toggle").click(function (e) {
+
+        // AddLocalStorage(this.sideBarstate);
+      
       var wrapper = $('.wrapper');
       wrapper.toggleClass("closed")
+      e.stopPropagation();
     })
   }
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { LocalStorageModule } from 'angular-2-local-storage';
 import { AppComponent } from './app.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-// import {NgSpinningPreloader} from 'ng2-spinning-preloader';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  {
+    path: 'about',
+    component: AboutComponent,
+    data: {title: 'About me'}
+  },
+  {
+    path:'projects',
+    component : ProductsComponent,
+    data:{title:'My projects'}
+  },
+  {
+    path:'contact',
+    component : ContactComponent,
+    data:{title:'Contact me'}
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProductsComponent,
+    ContactComponent  
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes,{ useHash: false }),
     AngularFontAwesomeModule,
-    // NgSpinningPreloader 
+    SlimLoadingBarModule,
+    LocalStorageModule.withConfig({
+      prefix: "storage",
+      storageType: "localStorage"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
